88e52fa0a63d424422e088a91203d966d6078b78
RL Tek <Rlusby@live.com>
2023-03-17 13:10:55 -0400

    added a search filter to the characters page

--- src/components/characters.js
+++ src/components/characters.js
@@ -9,6 +9,7 @@ import Burger from './burger'
 
 export default function Characters() {
     const [characters, setCharacters] = useState([])
+    const [search, setSearch] = useState('')
 
 
 //Calls the API to get a list of characters
@@ -16,6 +17,7 @@ export default function Characters() {
         fetch('https://bobsburgers-api.herokuapp.com/characters/')
         .then(res => res.json())
         .then(res => setCharacters(res))
+        .catch(err => console.log(err))
     }, [])
 
 //Maps through the characters and renders their image and name in a clickable link
@@ -26,6 +28,14 @@ export default function Characters() {
         </Link>
     </div>)
 
+//Filters and maps through the list of characters based on user search
+    const filteredCharacterList = characters?.filter(c => c.name.includes(search) || '').map(character => <div id="character-cards" key={character.id}>
+    <Link to={`/character/${character.id}`}>
+    <img src={character.image} alt="character" id="character-images"/>
+    <p>{character.name}</p>
+    </Link>
+</div>)
+
 //renders the Character page
     return(
     <div id="characters-page">
@@ -35,9 +45,12 @@ export default function Characters() {
             </header>
             <Navbar />
             <Burger />
+            <h3>Search for your favorite character!</h3>
+            <input type="text" value={search} onChange={e => setSearch(e.target.value)}/>
+            <p>Names are case sensitive!</p>
             <h2>Characters:</h2>
             <div id="characters-section">
-                {characterList}
+                {search === '' ? characterList : filteredCharacterList}
             </div>
     </div>
         
